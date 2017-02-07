b22a76c14800270bfdfe9c3f10c908bc559c3615
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-02-07 14:20:07 +0100

    added gitignore and very minor progress on lab5

--- lab-05/lab5_1.m
+++ lab-05/lab5_1.m
@@ -1,4 +1,22 @@
 %% LAB5_1
 clc; clear; close all;
 
-%% 
+plot_h = 2;
+plot_w = 2;
+
+%% load image and show original
+I = imread('rice.tif');
+dI = double(I)/255;
+subplot(plot_w, plot_h, 1); imshow(dI); title('original');
+
+%% choose a single grain of rice
+Icrop = imcrop(I);
+dIcrop = double(Icrop)/255;
+
+[yValuesIn_dIcrop, xValuesIn_dIcrop] = hist(dIcrop(:), 100);
+[yValuesIn_dI    , xValuesIn_dI    ] = hist(dI    (:), 100);
+
+figure,
+subplot(plot_w, plot_h, 2); 
+plot(xValuesIn_dI, yValuesIn_dI),
+title('original');
