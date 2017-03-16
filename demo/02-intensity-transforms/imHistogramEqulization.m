a2ce2197e2ee872f769afaa3da501ccdd575a179
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-16 21:09:17 +0100

    proper refactor

--- demo/02-intensity-transforms/imHistogramEqulization.m
+++ demo/02-intensity-transforms/imHistogramEqulization.m
@@ -3,38 +3,38 @@
 
 clc, clear, close all;
 
-RandomNumbers = round(99*rand(1000,1).^.3)+1;
-[YValues, XValues] = hist(RandomNumbers,100);
+randomNumbers = round(99*rand(1000,1).^.3)+1;
+[yValues, xValues] = hist(randomNumbers, 100);
 
 figure(1)
-plot(XValues, YValues, 'xb');
+plot(xValues, yValues, 'xb');
 title('Histogram for original data');
 pause;
 
 figure(2)
-Sum = sum(YValues);
-CummulativeSum = cumsum(YValues)/Sum;
-plot(XValues, CummulativeSum, '.r');
-axis([XValues(1),XValues(end), 0,1]);
+sumOfYValues = sum(yValues);
+cummulativeSum = cumsum(yValues)/sumOfYValues;
+plot(xValues, cummulativeSum, '.r');
+axis([xValues(1), xValues(end), 0,1]);
 title('Cumulated Density Function for original data');
 
-for k = 1:length(RandomNumbers)
-    y(k) = CummulativeSum(RandomNumbers(k));
+for k = 1:length(randomNumbers)
+    y(k) = sum(randomNumbers(k));
 end;
 
-[NewYValues, NewXValues]=hist(y,100);
+[newYValues, newXValues] = hist(y,100);
 pause;
 
 figure(3)
-plot(NewXValues, NewYValues, 'xb');
+plot(newXValues, newYValues, 'xb');
 title('Histogram for transformed data');
 pause;
 
 figure(4)
-NewSum = sum(NewYValues);
-NewCummulativeSum = cumsum(NewYValues)/NewSum;
-plot(NewXValues, NewCummulativeSum, '.r');
-axis([NewXValues(1),NewXValues(end), 0,1]);
+newSumOfYValues = sum(newYValues);
+newCummulativeSum = cumsum(newYValues)/newSumOfYValues;
+plot(newXValues, newCummulativeSum, '.r');
+axis([newXValues(1), newXValues(end), 0,1]);
 title('Cumulative Density Function for transformerd data');
 
 pause; close all;
