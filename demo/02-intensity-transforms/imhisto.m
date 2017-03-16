87e955864969936dd6dbaeb970940492577de792
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-16 19:39:13 +0100

    minor change

--- demo/02-intensity-transforms/imhisto.m
+++ demo/02-intensity-transforms/imhisto.m
@@ -1,19 +1,18 @@
 %Histogram equalization, what does it?
 %HKa Feb. 2015
 
-%Image = imread('cameraman.tif');
+Image = imread('cameraman.tif');
 %Image = imread('lenna.tif');
 %Image = imread('fingerprint.tif');
-Image = imread('kobling.tif');
+%Image = imread('kobling.tif');
+
+ImageWithEqualizedHistogram = histeq(Image);
 
 figure(1)
 subplot(221); imshow(Image)
 subplot(222); hist(double(Image(:)),50)
+
 pause;
 
-subplot(223)
-ImageWithEqualizedHistogram = histeq(Image);
-imshow(uint8(ImageWithEqualizedHistogram));
-
-subplot(224); 
-hist(double(ImageWithEqualizedHistogram(:)),50)
+subplot(223); imshow(uint8(ImageWithEqualizedHistogram));
+subplot(224); hist(double(ImageWithEqualizedHistogram(:)),50)
