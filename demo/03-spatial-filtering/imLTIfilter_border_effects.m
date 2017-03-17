37aef130e05a0d3677e4eaed131000b2af0dd8ef
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-17 03:27:29 +0100

    started other refactor

--- demo/03-spatial-filtering/imLTIfilter_border_effects.m
+++ demo/03-spatial-filtering/imLTIfilter_border_effects.m
@@ -2,17 +2,17 @@
 %HKa Feb. 2015
 clear, close all;
 
-N=256;
+N = 256;
+
 %Impulse response or P(oint) S(pread) F(unction)
-h=[1 2 3;4 5 6;7 8 9];
+h = [1, 2, 3; 4, 5, 6; 7, 8, 9];
 
-d=zeros(N,N);
-d(1,1)=1;
+d = zeros(N, N);
+d(1,1) = 1;
 
-f=imfilter (d,h,'circular');
+f = imfilter (d, h, 'circular');
 
-h
 f(1:5, 1:5)
 f(end-5:end, 1:5)
-f(1:5,end-5:end)
-f(end-5:end,end-5:end)
+f(1:5, end-5:end)
+f(end-5:end, end-5:end)
