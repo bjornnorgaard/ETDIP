f83953667d864229feb89671613025d5960c151a
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-18 03:05:54 +0100

    quick refactor of immorphmaxcontrast almost done

--- demo/05-morphological-image-processing/imMorphMaxContrast.m
+++ demo/05-morphological-image-processing/imMorphMaxContrast.m
@@ -4,18 +4,25 @@
 
 clear, close all;
 
-%Read image and show
-I=imread('rice.tif');
-%I=imread('cameraman.tif')
-figure(1), imshow(I), title('Original');
+% Read image and show
+image = imread('rice.tif');
+%image = imread('cameraman.tif')
+figure(1), imshow(image), title('Original');
+pause;
 
 %Maximize Contrast
-se = strel('disk', 10);
-Itop = imtophat(I, se); %h=f - (f open b)
-Ibot = imbothat(I, se); %%h=(f close b) -f
+structuralElement = strel('disk', 10);
+Itop = imtophat(image, structuralElement); % h=f - (f open b)
+Ibot = imbothat(image, structuralElement); % h=(f close b) -f
 figure(2), imshow(Itop), title('top-hat image');
-figure(3), imshow(Ibot), title('bottom-hat image');
+pause;
 
-%Subtract Images
-Ienhance = imsubtract(imadd(Itop, I), Ibot);
+figure(3), imshow(Ibot), title('bottom-hat image');
+pause;
+
+% Subtract Images
+Ienhance = imsubtract(imadd(Itop, image), Ibot);
 figure(4), imshow(Ienhance), title('Original + top-hat - bottom-hat');
+pause;
+
+close all;
