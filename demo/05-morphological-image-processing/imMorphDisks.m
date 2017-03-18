1adf1170743c67a13062f90c7c62b68ff2f0ab89
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-18 03:02:05 +0100

    refactor of immorphdiskes done

--- demo/05-morphological-image-processing/imMorphDisks.m
+++ demo/05-morphological-image-processing/imMorphDisks.m
@@ -3,35 +3,41 @@
 
 clear, close all;
 
-%Lets make som small squares, random size and location
-N=256;
-I=zeros(N,N);
+% Lets make som small squares, random size and location
+N = 256;
+Image = zeros(N, N);
 
-p=.005; %Parameter to set "power" in noise
-x = rand(size(I));
-I(find(x >= p/2 & x < p)) = 1; % Maximum (saturated) value
+p=.005; % Parameter to set "power" in noise
+x = rand(size(Image));
+Image(find(x >= p/2 & x < p)) = 1; % Maximum (saturated) value
 
-[r,s]=find(I~=0);
-R=round(4*rand(1,length(r)));%Get square size
-for k=1:length(r)
-    for m=0:R(k)-1
-        for n=0:R(k)-1
-            I(r(k)+m,s(k)+n)=1;
+[r,s] = find(Image ~= 0);
+R = round(4*rand(1,length(r))); %Get square size
+
+for k = 1:length(r)
+    for m = 0:R(k)-1
+        for n = 0:R(k)-1
+            Image(r(k)+m,s(k)+n)=1;
         end
     end
 end
 
-bI=logical(I);
-figure, imshow(bI);title('Original')
+bI = logical(Image);
+figure, imshow(bI); title('Original')
 pause;
 
-%Ltes find the squares with side length larger than 4 
-se = strel('square', 4);
-bI1= imerode(bI,se);
-figure, imshow(bI1);title('Erodered original')
+% Lets find the squares with side length larger than 4 
+structuralElement = strel('square', 4);
 
-bI2= imdilate(bI1,se);
-figure, imshow(bI2), title('Erosion followed by dilation of original (opnening)')
+bI1 = imerode(bI, structuralElement);
+figure, imshow(bI1); title('Erodered original')
+pause;
 
-sum(bI2(:))/(4*4)
-length(find(R==4))
+bI2 = imdilate(bI1, structuralElement);
+figure, imshow(bI2), title('Erosion followed by dilation of original (opening)')
+
+sum(bI2(:)) / (4*4)
+length(find(R == 4))
+
+pause;
+close all;
