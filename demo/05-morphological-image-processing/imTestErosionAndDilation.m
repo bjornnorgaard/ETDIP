e7bcf22359bbd14f66e2c72307361d5f94b2ba5e
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-18 03:15:33 +0100

    added ANOTHER missing image and refactored file

--- demo/05-morphological-image-processing/imTestErosionAndDilation.m
+++ demo/05-morphological-image-processing/imTestErosionAndDilation.m
@@ -4,19 +4,28 @@
 clear, close all;
 
 %Load
-bw = imread('henrik.bmp');
+image = imread('henrik.bmp');
 
 %Use negative in the following
-bw1=1-bw;
+bw1 = 1 - image;
 
 %Do morphology
-se = strel('line',14,0);
+se = strel('line', 14, 0);
 bw2 = imerode(bw1, se);
-bw3 = imdilate(bw1,se);
-bw4 = imclose (bw1,se);
+bw3 = imdilate(bw1, se);
+bw4 = imclose(bw1, se);
+bw5 = imopen(bw1, se);
 
 %Show results
 figure(1), imshow(bw1), title('Original negative')
+pause;
 figure(2), imshow(bw2), title('Erosion of negative')
+pause;
 figure(3), imshow(bw3), title('Dilation of negative')
+pause;
 figure(4), imshow(bw4), title('Closing of negative')
+pause;
+figure(5), imshow(bw5), title('Opening of negative')
+pause;
+
+close all;
