0f954c723f03ef766ffbcc38d79abc75584f1551
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-18 02:56:35 +0100

    refactored imgetboundaryusingmorph

--- demo/05-morphological-image-processing/imGetBoundaryUsingMorph.m
+++ demo/05-morphological-image-processing/imGetBoundaryUsingMorph.m
@@ -1,22 +1,23 @@
 %Find boundry using Morphological operations
 %HKa Jan. 2015
 
-clear, close all;
+clc; clear; close all;
 
-%Square as test image
-% bw=zeros(256,256);
-% bw(50:150,50:150)=1;
+% Square as test image
+% blackAndWhiteImage = zeros(256, 256);
+% blackAndWhiteImage(50:150, 50:150) = 1;
 
-%Rice as test image
-I=imread('rice.tif');
+% Rice as test image
+riceImage = imread('rice.tif');
 
-dI=double(I)/255;
-bw=zeros(size(I));
-r=find(dI>0.43);
-bw(r)=1;
+riceImageDouble = double(riceImage) / 255;
+blackAndWhiteImage = zeros(size(riceImage));
+indexForThresholding = find(riceImageDouble > 0.43);
+blackAndWhiteImage(indexForThresholding) = 1;
 
-se = strel('disk',2);
-bw1 = imerode(bw,se);
-bw2=bw-bw1;
-figure(1), imshow(bw), title('Original (thresholded version)');
-figure(2), imshow(bw2), title('Boundary')
+structuralElement = strel('disk', 2);
+bw1 = imerode(blackAndWhiteImage, structuralElement);
+bw2 = blackAndWhiteImage - bw1;
+
+figure(1), imshow(blackAndWhiteImage), title('Original (thresholded version)');
+figure(2), imshow(bw2), 			   title('Boundary')
