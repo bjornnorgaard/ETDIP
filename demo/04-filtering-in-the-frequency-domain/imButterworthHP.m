72e74b5834d01284d3325caa2aaf9b8a6b1dd6d5
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-20 23:07:52 +0100

    minor update

--- demo/04-filtering-in-the-frequency-domain/imButterworthHP.m
+++ demo/04-filtering-in-the-frequency-domain/imButterworthHP.m
@@ -3,12 +3,13 @@
 %Notice minimum ringing
 %HKa Feb. 2015
 
-clear, close all;
+clc; clear; close all;
 
-%Load originalImage and show
+%Load image
 originalImage = imread('cameraman.tif');
 %originalImage = zeros(256,256); originalImage(125:130,10:246)=255;
-%Create rektangel
+
+% Create rektangel
 image = double(originalImage)/255;
 
 figure(1), imshow(originalImage); title('Original image')
@@ -19,7 +20,7 @@ FI = fft2(image);
 FI = fftshift(FI);
 
 % Butterworth HP filter
-n = 1;
+n = 2;
 D0 = 25;
 for r = 1:size(FI,1)
     for s = 1:size(FI,2)
@@ -28,12 +29,15 @@ for r = 1:size(FI,1)
 end
 
 logabsFI = log10(abs(FI));
+%logabsFI = abs(FI);
+
 figure(2), imshow(logabsFI/max(logabsFI(:))); title('Fourier of cutted frequence using Butterworth')
 pause;
 
 % Lets return to the spatial domain
 FI = fftshift(FI);
 idJ = real(ifft2(FI));
+
 figure(3), imshow(idJ/max(idJ(:))); title('invers Fourier of original image filtered using Butterworth');
 pause;
 
