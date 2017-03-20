c331b71ab7ad3656093f7dcab9a98382a29fbff5
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-20 22:54:53 +0100

    begun refactor of demo for butterworth hp

--- demo/04-filtering-in-the-frequency-domain/imButterworthHP.m
+++ demo/04-filtering-in-the-frequency-domain/imButterworthHP.m
@@ -5,29 +5,36 @@
 
 clear, close all;
 
-%Load image and show
-I=imread('cameraman.tif');
+%Load originalImage and show
+originalImage = imread('cameraman.tif');
+%originalImage = zeros(256,256); originalImage(125:130,10:246)=255;
 %Create rektangel
-%I=zeros(256,256); I(125:130,10:246)=255;
-dI=double(I)/255;
-figure(1), imshow(I);title('Original image')
+image = double(originalImage)/255;
 
-%Lets calculate the Fourier Transform (goto Frequnecy Domain)
-FI=fft2(dI);
-FI=fftshift(FI);
+figure(1), imshow(originalImage); title('Original image')
+pause;
 
-%Butterworth HP filter
-n=2;
-D0=25;
-for r=1:size(FI,1)
-    for s=1:size(FI,2)
-        FI(r,s)=1/(1+(((r-128)^2+(s-128)^2)/D0^2)^(-n))*FI(r,s);
+% Lets calculate the Fourier Transform (goto Frequnecy Domain)
+FI = fft2(image);
+FI = fftshift(FI);
+
+% Butterworth HP filter
+n = 1;
+D0 = 25;
+for r = 1:size(FI,1)
+    for s = 1:size(FI,2)
+        FI(r,s) = 1/(1+(((r-128)^2+(s-128)^2)/D0^2)^(-n))*FI(r,s);
     end
 end
-logabsFI=log10(abs(FI));
-figure(2), imshow(logabsFI/max(logabsFI(:)));title('Fourier of cutted frequence using Butterworth')
 
-%Lets return to the spatial domain
-FI=fftshift(FI);
-idJ=real(ifft2(FI));
-figure(3), imshow(idJ/max(idJ(:)));title('invers Fourier of original image filtered using Butterworth');
+logabsFI = log10(abs(FI));
+figure(2), imshow(logabsFI/max(logabsFI(:))); title('Fourier of cutted frequence using Butterworth')
+pause;
+
+% Lets return to the spatial domain
+FI = fftshift(FI);
+idJ = real(ifft2(FI));
+figure(3), imshow(idJ/max(idJ(:))); title('invers Fourier of original image filtered using Butterworth');
+pause;
+
+close all;
