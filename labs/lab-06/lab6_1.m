ed21bec4b294bd4358f0e5c0af123b6b5fdcb5d4
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-21 00:08:54 +0100

    added missing img and minor refactor

--- labs/lab-06/lab6_1.m
+++ labs/lab-06/lab6_1.m
@@ -4,45 +4,51 @@
 
 clear, close all;
 
-B=imread('fig5_16(a).jpg');
-dB=double(B)/255;
+B = imread('fig5_16(a).jpg');
+dB = double(B)/255;
 
-figure, imshow(B);title('Original');
+figure, imshow(B); title('1. Original');
+pause;
 
-FB=fftshift(fft2(dB));
-laFB=log10(abs(FB)+1);
-figure, imshow(laFB/max(laFB(:))),[x,y,p]=impixel;title('Fourier trans. of original')
+FB = fftshift(fft2(dB));
+laFB = log10(abs(FB)+1);
+figure, imshow(laFB/max(laFB(:))),[x,y,p]=impixel; title('2. Fourier trans. of original')
 
 %We use ideal bandrejectfilter
- Br=2;Bs=3;
- M=size(FB,1);
- N=size(FB,2);
- for m=1:M
-     for n=1:N
-       if (((m-M/2)^2+(n-N/2)^2)>((y(1)-M/2)^2+(x(1)-N/2)^2))&(((m-M/2)^2+(n-N/2)^2)<(y(2)-M/2)^2+(x(2)-N/2)^2) FB(m,n)=0; end;
-   end;
+Br = 2;Bs = 3;
+M = size(FB,1);
+N = size(FB,2);
+for m = 1:M
+	for n = 1:N
+		if (((m-M/2)^2+(n-N/2)^2)>((y(1)-M/2)^2+(x(1)-N/2)^2))&(((m-M/2)^2+(n-N/2)^2)<(y(2)-M/2)^2+(x(2)-N/2)^2) 
+			FB(m,n)=0;
+		end;
+	end;
 end;
 
-laFB=log10(abs(FB)+1);
-figure, imshow(real(laFB/max(laFB(:))));;title('Fourier trans. after reject filter');
+laFB = log10(abs(FB)+1);
+figure, imshow(real(laFB/max(laFB(:)))); title('3. Fourier trans. after reject filter');
 
 dBx=(ifft2(fftshift(FB)));
-figure, imshow(real(dBx));;title('Fourier trans. after reject filter');
+figure, imshow(real(dBx)); title('4. Fourier trans. after reject filter');
 
-E=dB-dBx;
-figure, imshow(histeq(abs(E/max(E(:)))));title('Removed signal')
+E = dB-dBx;
+figure, imshow(histeq(abs(E/max(E(:))))); title('5. Removed signal')
 
 %%Note Optional: Implementation of the Optium Notch Filter on pp. 338-342 in GW
-S=1/9*[1 1 1;1 1 1;1 1 1]; %Mask for block operation
+S = 1/9*[1 1 1;1 1 1;1 1 1]; %Mask for block operation
 %Uses formulas on pp. 341-342 in GW
-K=dB.*E;
-M1=imfilter(K,S);
-M2=imfilter(dB,S);
-M3=imfilter(E,S);
-M4=imfilter(E.^2,S);
-M5=M3.^2;
+K = dB.*E;
+M1 = imfilter(K,S);
+M2 = imfilter(dB,S);
+M3 = imfilter(E,S);
+M4 = imfilter(E.^2,S);
+M5 = M3.^2;
 
 W=(M1-M2.*M3)./(M4-M5);
 
-F_hat=real(dB-W.*E);
-figure, imshow(F_hat);title('Optimum notch GW pp. 338-342');
+F_hat = real(dB-W.*E);
+figure, imshow(F_hat); title('6. Optimum notch GW pp. 338-342');
+
+pause;
+close all;
--- labs/lab-06/lab6_2.m
+++ labs/lab-06/lab6_2.m
@@ -9,17 +9,21 @@ clear, close all;
 %I=imread('lena.jpg');
 I=imread('cameraman.tif');
 
-
 %dI=double(rgb2gray(I))/255; %Used for rose and lena
 dI=double(I)/255;%Used for cameraman
 figure, imshow(dI);title('Original image')
+pause;
 
 %DFT of image
 FI=fftshift(fft2(dI));
 logabsFI=log10(abs(FI)+1);
 figure, imshow(logabsFI/max(logabsFI(:)));title('Fourier of original image')
+pause;
 
 %Find d
 w=1:size(I,1);
 s=20*log10((w-size(I,1)/2+.01).^-1.6)+50;       %d=-1.6 is OK.
 figure, plot(w,20*logabsFI(:,size(I,1)/2),w,s );%Power in a horizontal-direction
+pause;
+
+close all;
